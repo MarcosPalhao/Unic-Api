76ae8d82f22992ca54d2e6db56811ab9198a5af5
Marcos Vinicius <marcos.palhao18@gmail.com>
2022-11-03 08:26:14 -0300

    feat: user registration

---
+++ src/modules/users/useCases/createUser/CreateUserUseCase.ts
@@ -0,0 +1,31 @@
+import { inject, injectable } from "tsyringe";
+import { ICreateUserDTO } from "@modules/users/dtos/ICreateUserDTO";
+import { IUsersRepository } from "@modules/users/repositories/IUsersRepository";
+import { AppError } from "@shared/errors/AppError";
+import { hash } from "bcryptjs";
+
+@injectable()
+class CreateUserUseCase {
+    constructor(
+        @inject("UsersRepository")
+        private usersRepository: IUsersRepository
+    ) { }
+
+    async execute({ name, email, password }: ICreateUserDTO): Promise<void> {
+        const userAlreadyExists = await this.usersRepository.findByEmail(email);
+
+        if (userAlreadyExists) {
+            throw new AppError("User already exists");
+        }
+
+        const passwordHash = await hash(password, 8);
+
+        await this.usersRepository.create({
+            name, 
+            email,
+            password: passwordHash
+        });
+    }
+}
+
+export { CreateUserUseCase };
