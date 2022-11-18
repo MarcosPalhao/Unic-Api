8e2bf81f0eb21af796797a95ad2925a6b5c9a8dc
Marcos Vinicius <marcos.palhao18@gmail.com>
2022-11-18 14:08:19 -0300

    feat: implementation for refresh token

--- src/shared/infra/http/routes/authenticate.routes.ts
+++ src/shared/infra/http/routes/authenticate.routes.ts
@@ -1,10 +1,13 @@
 import { AuthenticateUserController } from "@modules/users/useCases/authenticateUser/AuthenticateUserController";
+import { RefreshTokenController } from "@modules/users/useCases/refreshToken/RefreshTokenController";
 import { Router } from "express";
 
 const authenticateRoutes = Router();
 
 const authenticateUserController = new AuthenticateUserController();
+const refreshTokenController = new RefreshTokenController();
 
 authenticateRoutes.post("/sessions", authenticateUserController.handle);
+authenticateRoutes.post("/refresh-token", refreshTokenController.handle);
 
 export { authenticateRoutes };
